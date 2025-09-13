b1c69aeb9a41d85a75ce214d4f10d19a52be1b85
Yooshyasha <mejzikrejzi66@gmail.com>
2025-09-13 16:02:52 +0500

    feat

--- src/main/kotlin/com/yooshyasha/reactiveuserservice/service/UserService.kt
+++ src/main/kotlin/com/yooshyasha/reactiveuserservice/service/UserService.kt
@@ -3,6 +3,12 @@ package com.yooshyasha.reactiveuserservice.service
 import com.yooshyasha.reactiveuserservice.dto.UserDTO
 import com.yooshyasha.reactiveuserservice.entity.User
 import com.yooshyasha.reactiveuserservice.repo.UserRepository
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactive.awaitLast
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Service
 
@@ -18,12 +24,31 @@ class UserService(
         return UserDTO(user)
     }
 
-    suspend fun getAllUsers(): Collection<UserDTO> {
-        val users = userRepository.findAll()
-            .map {
-                UserDTO(it)
-            }.collectList().awaitSingle()
+    suspend fun getAllUsers(): Collection<UserDTO> = coroutineScope {
+//        val users = userRepository.findAll()
+//            .map {
+//                UserDTO(it)
+//            }.collectList().awaitSingle()
 
-        return users
+        // Код ниже исключительно для практики
+
+        val channel = Channel<User>()
+
+        launch {
+            userRepository.findAll().asFlow().collect {
+                channel.send(it)
+            }
+
+            channel.close()
+        }
+
+        val users = mutableListOf<UserDTO>()
+        launch {
+            channel.receiveAsFlow().collect {
+                users.add(UserDTO(it))
+            }
+        }
+
+        return@coroutineScope users
     }
 }
